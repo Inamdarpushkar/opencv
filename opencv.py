05977892c24c4a9685cd544b6eb311e465165658
Inamdarpushkar <inamdarpushkar27@gmail.com>
2018-11-17 20:10:03 -0800

    f

--- opencv.py
+++ opencv.py
@@ -60,34 +60,39 @@ import numpy as np
 
 
 ##PART III
-#adding different things on the image
-img=cv2.imread("_02_01.png",cv2.IMREAD_COLOR)
+# #adding different things on the image
+# img=cv2.imread("_02_01.png",cv2.IMREAD_COLOR)
+#
+# #adding line,rectangle,circle,poygon
+# cv2.line(img,(0,0),(150,150),(255,255,255),15)
+# cv2.rectangle(img,(15,25),(200,150),(0,255,0),5)
+# cv2.circle(img,(100,63),55,(0,255),-1)
+#
+# pts=np.array([[10,5],[20,30],[70,20],[50,10]],np.int32)
+# #pts=pts.reshape((-1,1,2))
+# cv2.polylines(img,[pts],True,(0,255,255),5)
+#
+# #adding text to the image
+# font=cv2.FONT_HERSHEY_SIMPLEX
+# cv2.putText(img,'Test',(0,130),font,0.5,(200,255,255),5,cv2.LINE_AA)
+#
+# cv2.imshow('img',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-#adding line,rectangle,circle,poygon
-cv2.line(img,(0,0),(150,150),(255,255,255),15)
-cv2.rectangle(img,(15,25),(200,150),(0,255,0),5)
-cv2.circle(img,(100,63),55,(0,255),-1)
+# IV
+img=cv2.imread('_02_01.png',cv2.IMREAD_COLOR)
+# img[55,55]=[255,255,255]
+# print(img[55,55])
 
-pts=np.array([[10,5],[20,30],[70,20],[50,10]],np.int32)
-#pts=pts.reshape((-1,1,2))
-cv2.polylines(img,[pts],True,(0,255,255),5)
 
-#adding text to the image
-font=cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img,'Test',(0,130),font,0.5,(200,255,255),5,cv2.LINE_AA)
+img[100:150,100:150]=[255,255,255]
+
+
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
 
 
 
