22a220692b46fa21c484374410dc8eb16d2be689
Inamdarpushkar <inamdarpushkar27@gmail.com>
2018-11-17 19:15:02 -0800

    opencv

--- opencv.py
+++ opencv.py
@@ -33,23 +33,69 @@ import numpy as np
 
 
 ##PART II
-
-cap=cv2.VideoCapture(0)
-while (True):
-    # Capture frame-by-frame
-    ret,frame = cap.read()
-    #Our operations on the frame come here
-
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',frame)
-    cv2.imshow('frame',gray)
-
-    #Display the resulting frame
-    if cv2.waitKey(1) & 0xFF==ord('q'):
-        break
-#When everthing done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+#
+# cap=cv2.VideoCapture(0)
+# fourcc=cv2.VideoWriter_fourcc(*'xvid')
+# out=cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+#
+#
+#
+# while (True):
+#     # Capture frame-by-frame
+#     ret,frame = cap.read()
+#     #Our operations on the frame come here
+#
+#     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+#     out.write(frame)
+#     cv2.imshow('frame',gray)
+#     cv2.imshow('frame',frame)
+#
+#     #Display the resulting frame
+#     if cv2.waitKey(1) & 0xFF==ord('q'):
+#         break
+# #When everthing done, release the capture
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
 
 
 ##PART III
+img=cv2.imread("_02_01.png",cv2.IMREAD_COLOR)
+
+cv2.line(img,(0,0),(150,150),(255,255,255),15)
+cv2.rectangle(img,(15,25),(200,150),(0,255,0),5)
+cv2.circle(img,(100,63),55,(0,255),-1)
+
+pts=np.array([[10,5],[20,30],[70,20],[50,10]],np.int32)
+pts=pts.reshape((-1,1,2))
+cv2.polylines(img,[pts],True,(0,255,255),5)
+
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
