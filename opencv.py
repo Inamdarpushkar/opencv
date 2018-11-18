b21ba9fbdf2b8f9d26f9c9d33d8ffc6012f8fbb3
Inamdarpushkar <inamdarpushkar27@gmail.com>
2018-11-17 19:30:31 -0800

    opencv

--- opencv.py
+++ opencv.py
@@ -60,16 +60,21 @@ import numpy as np
 
 
 ##PART III
+#adding different things on the image
 img=cv2.imread("_02_01.png",cv2.IMREAD_COLOR)
 
+#adding line,rectangle,circle,poygon
 cv2.line(img,(0,0),(150,150),(255,255,255),15)
 cv2.rectangle(img,(15,25),(200,150),(0,255,0),5)
 cv2.circle(img,(100,63),55,(0,255),-1)
 
 pts=np.array([[10,5],[20,30],[70,20],[50,10]],np.int32)
-pts=pts.reshape((-1,1,2))
+#pts=pts.reshape((-1,1,2))
 cv2.polylines(img,[pts],True,(0,255,255),5)
 
+#adding text to the image
+font=cv2.FONT_HERSHEY_SIMPLX
+cv2.putText(img,'Test',(0,140),font,0.5,(200,255,255),2,cv2.LINE_AA)
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
