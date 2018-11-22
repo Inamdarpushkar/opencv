a5c996771540c34b529d70e8d6799fe8cb9076aa
Inamdarpushkar <inamdarpushkar27@gmail.com>
2018-11-22 07:02:15 -0800

    n

--- opencv_threshold.py
+++ opencv_threshold.py
@@ -12,11 +12,16 @@ retval,threshold=cv2.threshold(img,12,255,cv2.THRESH_BINARY)
 #converting to grayscale image
 grayscaled=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 retval,threshold2=cv2.threshold(grayscaled,12,255,cv2.THRESH_BINARY)
+
+#adaptive threshold (adjust threshold based on the global DM values)
+gaus=cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,155,1)
+
+
 #displaying images
 cv2.imshow('original',img)
 cv2.imshow('threshold',threshold)
 cv2.imshow('threshold2',threshold2)
-
+cv2.imshow('Gaussian',gaus)
 #keyboard connection press any key to destroyAllWindows
 cv2.waitKey(0)
 cv2.destroysallwindows()
