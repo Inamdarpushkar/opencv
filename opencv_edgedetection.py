e3a8600ed00f49c097a1426e313c544c4a504ca5
Inamdarpushkar <inamdarpushkar27@gmail.com>
2018-11-24 06:18:05 -0800

    f

--- opencv_edgedetection.py
+++ opencv_edgedetection.py
@@ -11,10 +11,13 @@ while True:
     sobelx=cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
     sobely=cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5)
 
+    edges=cv2.Canny(frame,100,120)
+
     cv2.imshow('Original',frame)
     cv2.imshow('Laplacian',laplacian)
     cv2.imshow('sobelx',sobelx)
     cv2.imshow('sobely',sobely)
+    cv2.imshow('edges',edges)
 
 
     k=cv2.waitKey(5) & 0xFF
